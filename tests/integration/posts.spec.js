054053434f346906105c4c3780c1e0bff9413f14
Vitor Pereira <vitorpereira@Vitors-MacBook-Pro.local>
2018-09-13 19:54:19 -0300

    Change schema

--- tests/integration/posts.spec.js
+++ tests/integration/posts.spec.js
@@ -1,9 +1,9 @@
-// import { expect } from 'chai';
-// import request from 'supertest';
-// import app from '../../src/app';
-// import { generateDatas } from '../../src/utils/fakerDatas';
+import { expect } from 'chai';
+import request from 'supertest';
+import app from '../../src/app';
+import { generateDatas } from '../../src/utils/fakerDatas';
 
-// describe('API Posts endpoints', () => {
+describe('API Posts endpoints', () => {
   // it('should be populate posts collection', (done) => {
   //   for (let i = 0; i < 1000; i++) {
   //     request(app)
@@ -15,22 +15,22 @@
   //         // const errorStack = JSON.parse(res.error.text);
   //         expect(res.statusCode).to.equal(200);
   //         expect(res.clientError).to.be.false;
-  //         done();
   //       });
-  //   }
+  //     }
+  //     done();
   // });
 
-  // it('should be list all posts', (done) => {
-  //   request(app)
-  //     .post('/v1/posts/find')
-  //     .set('Accept', 'application/json')
-  //     .expect('Content-Type', '/json/')
-  //     .send({})
-  //     .end((err, res) => {
-  //       // const errorStack = JSON.parse(res.error.text);
-  //       expect(res.statusCode).to.equal(200);
-  //       expect(res.clientError).to.be.false;
-  //       done();
-  //     });
-  // });
-// });
+  it('should be list all posts', (done) => {
+    request(app)
+      .post('/v1/posts/find')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', '/json/')
+      .send({})
+      .end((err, res) => {
+        // const errorStack = JSON.parse(res.error.text);
+        expect(res.statusCode).to.equal(200);
+        expect(res.clientError).to.be.false;
+        done();
+      });
+  });
+});
