6506efa16b2749717f719be561a38ad7a5eee053
Vitor Pereira <vitorpereira@Vitors-MacBook-Pro.local>
2018-09-13 21:17:11 -0300

    Identation of types

--- src/v1/services/posts/graph/types.js
+++ src/v1/services/posts/graph/types.js
@@ -1,25 +1,25 @@
 export default `
- type Post {
-   _id: String
-   title: String
-   author: String
-   post: String
-   description: String
- }
+  type Post {
+    _id: String
+    title: String
+    author: String
+    post: String
+    description: String
+  }
 
- input PostInput {
-   title: String
-   author: String
-   post: String
-   description: String
- }
+  input PostInput {
+    title: String
+    author: String
+    post: String
+    description: String
+  }
 
- type Query {
-   post(_id: String): Post
-   posts: [Post]
- }
+  type Query {
+    post(_id: String): Post
+    posts: [Post]
+  }
 
- type Mutation {
-   createPost(input: PostInput) : Post
- }
+  type Mutation {
+    createPost(input: PostInput) : Post
+  }
 `;
