b86acfb1a80e8c74daf6508f1473bd3467b0d31b
Vitor Pereira <vitorpereira@Vitors-MacBook-Pro.local>
2018-09-15 12:57:35 -0300

    Change schema

--- src/v1/services/posts/graph/types.js
+++ src/v1/services/posts/graph/types.js
@@ -1,17 +1,77 @@
+// export default `
+//   type Post {
+//     _id: String
+//     title: String
+//     author: String
+//     post: String
+//     description: String
+//   }
+
+//   input PostInput {
+//     title: String
+//     author: String
+//     post: String
+//     description: String
+//   }
+
+//   type Query {
+//     post(_id: String): Post
+//     posts: [Post]
+//   }
+
+//   type Mutation {
+//     createPost(input: PostInput) : Post
+//   }
+// `;
+
 export default `
   type Post {
     _id: String
-    title: String
-    author: String
-    post: String
+    name: String
+    photos: [String]
     description: String
+    registerNumber: Int
+    color: String
+    valid: Boolean
+    category: String
+    lot: String
+    price: Int
+    brand: String
+    origin: String
+    plots: Int
+    freight: Int
+    evaluation: Int
+    barCode: String
+    model: String
+    weight: Int
+    warranty: Int
+    provider: String
+    code: String
+    stores: [String]
   }
 
   input PostInput {
-    title: String
-    author: String
-    post: String
+    name: String
+    photos: [String]
     description: String
+    registerNumber: Int
+    color: String
+    valid: Boolean
+    category: String
+    lot: String
+    price: Int
+    brand: String
+    origin: String
+    plots: Int
+    freight: Int
+    evaluation: Int
+    barCode: String
+    model: String
+    weight: Int
+    warranty: Int
+    provider: String
+    code: String
+    stores: [String]
   }
 
   type Query {
--- src/v1/services/posts/schema.js
+++ src/v1/services/posts/schema.js
@@ -1,19 +1,50 @@
 import { Joi } from 'celebrate';
 
+// const post = {
+//   title: Joi.string().required(),
+//   author: Joi.string().required(),
+//   post: Joi.string().required(),
+//   description: Joi.string().required()
+// }
+
 const post = {
-  title: Joi.string().required(),
-  author: Joi.string().required(),
-  post: Joi.string().required(),
-  description: Joi.string().required()
+  name: Joi.string().required(),
+  photos: Joi.array().required(),
+  description: Joi.string().required(),
+  registerNumber: Joi.number().required(),
+  color: Joi.string().required(),
+  valid: Joi.boolean().required(),
+  category: Joi.string().required(),
+  lot: Joi.string().required(),
+  price: Joi.number().required(),
+  brand: Joi.string().required(),
+  origin: Joi.string().required(),
+  plots: Joi.number().required(),
+  freight: Joi.number().required(),
+  evaluation: Joi.number().required(),
+  barCode: Joi.string().required(),
+  model: Joi.string().required(),
+  weight: Joi.number().required(),
+  warranty: Joi.number().required(),
+  provider: Joi.string().required(),
+  code: Joi.string().required(),
+  stores: Joi.array().required()
 }
 
 export const create = {
   body: Joi.object().keys(post)
 };
 
+// export const find = {
+//   body: Joi.object().keys({
+//     _id: Joi.string(),
+//     author: Joi.string()
+//   })
+// };
+
 export const find = {
   body: Joi.object().keys({
     _id: Joi.string(),
-    author: Joi.string()
+    registerNumber: Joi.number()
   })
 };
